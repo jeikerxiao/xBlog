a10aa9f7d265d9ecbb118b8536f0bc5950f8ae4b
jeikerxiao <jeiker@126.com>
2018-03-13 15:55:09 +0800

    init project
    init project

---
+++ src/main/java/com/jeiker/xblog/XblogApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.xblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XblogApplication.class, args);
+	}
+}
